dbbf6e7b79511c6a86054259fd54e1e15db0b915
AlejandroRB95 <22394061@utcancun.edu.mx>
2025-03-24 17:27:46 -0500

    amplifymain

---
+++ amplify_city_box/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifycitybox646c0efc": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "Directorio": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
